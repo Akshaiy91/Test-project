dcde3b64743db5d53ae5be61ff864f694373d9ca
Akshaiy Narayanan <akshaiynarayanan@gmail.com>
2022-02-11 13:12:44 +1300

    Changes made

---
+++ checkversion.sh
@@ -0,0 +1,3 @@
+webpage=$(curl -L https://raw.githubusercontent.com/NeuroDesk/neurodesk.github.io/hugo-docsy/data/neurodesktop.toml?fbclid=IwAR3ZKAAu-Q1nbxXdoLglPUOIg6Gm07rBdD-Zg-DBkdocd8Y0lOo4vkAKNTo | head -n 1)
+echo $webpage
+
